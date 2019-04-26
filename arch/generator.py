ad6a1130b5add1e7e58fdb6e9b396f05ef521686
Scope <yakudza228@gmail.com>
2019-04-26 19:38:47 +0500

    Some changes in logic

--- arch/generator.py
+++ arch/generator.py
@@ -3,6 +3,13 @@ import random
 import os
 
 
+# TODO '''
+# Просится такая декомпозиция:
+#
+# Нечто, чистящее текст и разбивающее его по предложениям Нечто, принимающее
+# на вход список предложений и выдаёт некоторый набор предложений для набора
+#
+# '''
 class Generator:
     @staticmethod
     def generate_text(file):
@@ -29,6 +36,24 @@ class Generator:
         return result
 
     @staticmethod
+    def split_text(text: str):
+        sent = text.replace('!', '.') \
+            .replace('\n', '.') \
+            .replace('?', '.') \
+            .split('.')
+        for s in sent:
+            if not s or s.startswith('– ') or s.startswith(
+                    ' ') or s.startswith('\t'):
+                sent.remove(s)
+        return sent
+
+    @staticmethod
+    def clear_text(text):
+        text.replace('«', "\"") \
+            .replace('–', '-') \
+            .replace('»', '\"')
+
+    @staticmethod
     def generate_json(text):
         return json.dumps(text, ensure_ascii=False)
 
@@ -36,23 +61,22 @@ class Generator:
     def write_json(file, filename):
         with open('./texts/json/{}.json'.format(filename), 'w') as f:
             f.write(Generator.generate_json(Generator.generate_text(file)))
-        with open('./texts/all_texts.json') as f:
-            js = f.read()
-            if js:
-                jsons = json.loads(js)
+        try:
+            with open('./texts/all_texts.json') as f:
+                jsons = json.load(f)
                 if filename not in jsons:
                     jsons.append(filename)
-            else:
-                jsons = [filename]
+        except FileNotFoundError:
+            jsons = [filename]
         with open('./texts/all_texts.json', 'w') as f:
             f.write(json.dumps(jsons))
 
     @staticmethod
     def get_texts_names():
-        with open('./texts/all_texts.json') as f:
-            js = f.read()
-            if js:
-                return json.loads(js)
+        try:
+            with open('./texts/all_texts.json') as f:
+                return json.load(f)
+        except FileNotFoundError:
             return []
 
     @staticmethod
@@ -65,7 +89,7 @@ class Generator:
     def get_random_string():
         text = Generator.get_random_text()
         with open('./texts/json/{}.json'.format(text)) as f:
-            strings = json.loads(f.read())
+            strings = json.load(f)
             return random.choice(strings)
 
     @staticmethod
